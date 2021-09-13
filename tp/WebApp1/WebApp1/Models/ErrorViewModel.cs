c0064bb0863f2c7cbc30a1c37934aef2ba7fce44
Damien Carrier <damien.carrier@mines-ales.org>
2021-09-13 12:03:31 +0200

    ajout du cours

---
+++ tp/WebApp1/WebApp1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
