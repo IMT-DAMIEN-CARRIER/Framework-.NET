89586e04cedbe44707f98e5668b7fc2025965445
Damien Carrier <damien.carrier@mines-ales.org>
2021-09-15 15:48:21 +0200

    Projet Blog : front

--- tp_noté/BlogBlazor/BlogBlazor/Client/obj/Debug/net5.0/serviceworkers/wwwroot/service-worker.js
+++ tp_noté/BlogBlazor/BlogBlazor/Client/obj/Debug/net5.0/serviceworkers/wwwroot/service-worker.js
@@ -2,4 +2,4 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
-/* Manifest version: HhGO/010 */
+/* Manifest version: bTm0IXXp */
